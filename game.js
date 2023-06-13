3e0fc75f6a11cfccfa09bb1a5b8c94fb36dea8ae
cut-copy-paste <ambition.aidev@gmail.com>
2023-06-12 20:58:18 -0500

    added code

--- game.js
+++ game.js
@@ -1,10 +1,20 @@
 console.log("Hello RPS!")
 
+
+var wins = 0,
+    losses = 0,
+    ties = 0
+    choices = ["rock", "Paper", "Scissors"]
+
 // human chooses R, P, S
+// var humanChoice = prompt("Choose Rock, Paper, Scissors")
+// console.log(humanChoice)
     //get choice from users input
     // computer randomly chooses
+var randomNum = Math.floor( Math.random() * 3)
+console.log(randomNum)
 
-// compare choices
+// computer choices
     // if humanChoice is S and pcChoice is P OR
     // if humanChoice is R and pdChoice is S OR
     // if humanChoice is P and pcChoice is R
@@ -23,4 +33,3 @@ console.log("Hello RPS!")
         // if yes, restart game
         // if no, end game - close application.
 
-        
