7a1eb08e8eb9b051ff6f4016e53dd6e05ab1be4f
cut-copy-paste <ambition.aidev@gmail.com>
2023-06-12 20:36:07 -0500

    add pseduo code

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
 console.log("Hello RPS!")
+
+// human chooses R, P, S
+    //get choice from users input
+    // computer randomly chooses
+
+// compare choices
+    // if humanChoice is S and pcChoice is P OR
+    // if humanChoice is R and pdChoice is S OR
+    // if humanChoice is P and pcChoice is R
+        // wins
+        // alert that human wins
+        // increase wins
+    // if humanChoice === psChoice
+        // ties
+        // alert human tie
+        //increase tie
+    //else
+        // alert human had lost
+        // increase lossses
+
+    // ask user "do you want to play again?""
+        // if yes, restart game
+        // if no, end game - close application.
+
+        
